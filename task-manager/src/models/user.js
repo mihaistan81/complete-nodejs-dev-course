c8d66d9ecaad400fef7706b28f36557663b7cbbb
Mihail Stan <mihail.stan@SVROLP00894.ro.softvision.global>
2019-12-09 18:39:06 +0200

    Added the password hashing

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs') 
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -39,6 +40,18 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+userSchema.pre('save', async function(next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 // const me = new User({ name: '  Mihai  ', email: 'mike@VERSATILE.ro   ', password: 'phone1234' });
 // me.save().then( () => {
